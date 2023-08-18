56838814c8ed4430ff3f14df62f00cd91b8347f9
Izaan Sohail <izaansohail1999@gmail.com>
2023-08-18 23:17:50 +0500

    Test Commit

---
+++ app/Console/Commands/watch.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use JetBrains\PhpStorm\NoReturn;
+
+class watch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'watch';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Created For Testing Purposes';
+
+    /**
+     * Execute the console command.
+     */
+    #[NoReturn] public function handle(): void
+    {
+        dd("this is a test command");
+    }
+}
